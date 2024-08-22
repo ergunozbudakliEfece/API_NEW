e5a912d3ae226d89d5bc4a1318b82ee115ab3d15
surec.gelistirme2 <efece641488>
2024-08-22 09:49:35 +0300

    Stock Controller eklendi.

---
+++ Controllers/StockController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SQL_API.Context;
+using SQL_API.Wrappers.Abstract;
+using SQL_API.Wrappers.Concrete;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SQL_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _Context;
+        private readonly IConfiguration _configuration;
+        public StockController(ApplicationDbContext Context, IConfiguration configuration)
+        {
+            _Context = Context;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IResponse> GetStocks(string? Kapali = null)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+
+                string query = $@"EXEC SP_STOCKS";
+
+                string sqldataSource = _configuration.GetConnectionString("NOVA_EFECE")!;
+                SqlDataReader sqlreader;
+                await using (SqlConnection mycon = new SqlConnection(sqldataSource))
+                {
+                    mycon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, mycon))
+                    {
+                        sqlreader = await myCommand.ExecuteReaderAsync();
+                        table.Load(sqlreader);
+                        sqlreader.Close();
+                        mycon.Close();
+                    }
+                }
+                return new SuccessResponse<string>(JsonConvert.SerializeObject(table), "Başarılı");
+            }
+            catch (Exception Ex)
+            {
+                return new ErrorResponse(Ex);
+            }
+        }
+    }
+}
