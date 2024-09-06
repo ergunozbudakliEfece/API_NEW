afec0c01a8dc9787ec4f269cbf1966e53a476fe1
dincersipka-efece <dincersipka@efecegalvaniz.com>
2024-09-06 17:51:31 +0300

    Fabrika Stok Bakiyeleri eklendi.

---
+++ Controllers/SalesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SQL_API.Context;
+using SQL_API.Wrappers.Abstract;
+using SQL_API.Wrappers.Concrete;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SQL_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _Context;
+        private readonly IConfiguration _configuration;
+
+        public SalesController(ApplicationDbContext Context, IConfiguration configuration)
+        {
+            _Context = Context;
+            _configuration = configuration;
+        }
+
+        [HttpGet("GetFactoryStockBalances")]
+        public async Task<IResponse> GetFactoryStockBalances()
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = $@"EXEC SP_FACTORYBALANCE";
+
+                string sqldataSource = _configuration.GetConnectionString("NOVA_EFECE")!;
+                SqlDataReader sqlreader;
+                await using (SqlConnection mycon = new SqlConnection(sqldataSource))
+                {
+                    mycon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, mycon))
+                    {
+                        sqlreader = await myCommand.ExecuteReaderAsync();
+                        table.Load(sqlreader);
+                        sqlreader.Close();
+                        mycon.Close();
+                    }
+                }
+                return new SuccessResponse<string>(JsonConvert.SerializeObject(table), "Başarılı");
+            }
+            catch (Exception Ex)
+            {
+                return new ErrorResponse(Ex);
+            }
+        }
+    }
+}
